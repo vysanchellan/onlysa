cfe3623db476586980cbb298fcd0aef5a3da8966
chellanvysan <chellanvysan@gmail.com>
2026-05-30 12:19:58 +0000

    Configure Tailwind CSS directly in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
+
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.forEach((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf.forEach((oneOfRule) => {
+          if (
+            oneOfRule.use &&
+            oneOfRule.use.loader &&
+            oneOfRule.use.loader.includes('next-style-loader')
+          ) {
+            oneOfRule.use.options.postcss = {
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+            };
+          }
+        });
+      }
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
