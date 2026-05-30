e4ce918bd63725a53cb635ba16e76151cf5f9a9c
Vysan Chellan <chellanvysan@gmail.com>
2026-05-30 15:05:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,6 @@
-
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.forEach((rule) => {
-      if (rule.oneOf) {
-        rule.oneOf.forEach((oneOfRule) => {
-          if (
-            oneOfRule.use &&
-            oneOfRule.use.loader &&
-            oneOfRule.use.loader.includes('next-style-loader')
-          ) {
-            oneOfRule.use.options.postcss = {
-              plugins: [
-                require('tailwindcss'),
-                require('autoprefixer'),
-              ],
-            };
-          }
-        });
-      }
-    });
-    return config;
-  },
+  turbopack: {},
 };
 
 module.exports = nextConfig;
